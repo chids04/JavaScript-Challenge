b4c81d9c54e43a17939cf3bdbbe222002a9cbd40
chids <guyepic39@gmail.com>
2023-11-21 23:18:37 +0000

    first revision

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const stocks = require('./stocks')
 
 const app = express()
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/stocks', async (req, res) => {
+  try {
   const stockSymbols = await stocks.getStocks()
   res.send({ stockSymbols })
+  } catch (error){
+    res.status(500).send({error: error.message});
+  }
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
--- static/scripts.js
+++ static/scripts.js
@@ -17,6 +17,41 @@ function drawTriangle (apex1, apex2, apex3) {
   ctx.fill()
 }
 
+function responseHandler(response){
+  if(response.ok == false){
+    throw new Error('Error loading stocks / stock data')
+  }
+  return response.json()
+}
+
+async function getStockInfo(stock){
+  try{
+    const stockInfo = await fetch('http://localhost:3000/stocks/' + stock)
+    const stockData = await responseHandler(stockInfo)
+    await console.log("Here are the values for " + stock + " stock " + stockData)
+  }
+  catch(error){
+    console.error(error.message)
+  }
+}
+
+async function getStocks() {
+  try{
+    const response = await fetch('http://localhost:3000/stocks')
+    const responseData = await responseHandler(response);
+
+    const symbols = responseData.stockSymbols;
+    console.log("List of available stocks: " + symbols)
+    
+    for(let i = 0; i<symbols.length; i++){
+      await getStockInfo(symbols[i])
+    }
+  }
+  catch(error){
+    console.error(error.message);
+  }
+}
+getStocks()
 drawLine([50, 50], [50, 550])
 drawTriangle([35, 50], [65, 50], [50, 35])
 
